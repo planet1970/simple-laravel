0d4c8ca0ca1011530aba79fed63371493da33e8f
planet1970 <cantezcan70@gmail.com>
2021-11-02 18:52:48 +0300

    Quiz listeleme, fontawesome

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Controllers\Admin\QuizController;
+use App\Http\Controllers\Admin\QuizController;
 
 
 Route::get('/', function () {
@@ -14,8 +14,7 @@ Route::middleware(['auth', 'verified'])->get('/panel', function () {
 
 
 Route::group(['middleware'=>['auth','isAdmin'],'prefix'=>'admin'],function(){
-   //Route::get('deneme', function(){
-       //return "prefix testi";
-   //});
+   Route::resource('quizzes',QuizController::class);
+
 
 });
